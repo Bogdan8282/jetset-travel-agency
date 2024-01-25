b6264c1f56e649805d4997f0244cc2beda71adb1
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2024-01-25 20:01:32 +0300

    Header bug fixed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("#header");
 
-    window.addEventListener("scroll", function () {
-    if (window.scrollY > 0) {
-        header.classList.add("active");
-    } else {
-        header.classList.remove("active");
+    function toggleHeaderClass() {
+        if (window.scrollY > 0) {
+            header.classList.add("active");
+        } else {
+            header.classList.remove("active");
+        }
     }
-    });
+
+    toggleHeaderClass();
+
+    window.addEventListener("scroll", toggleHeaderClass);
+});
+
+$(document).ready(function () {
+  $(".feedbacks__slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: false,
+    variableWidth: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 600,
+    easing: "linear",
+    infinite: true,
+    initialSlide: 1,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true,
+    swipe: true,
+    touchTreshold: 8,
+    touchMove: true,
+    waitForAnimate: true,
+  });
 });
