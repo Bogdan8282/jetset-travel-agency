8a3de786cc7e3374d092a7ce55458d8ae8529b5a
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2024-01-27 15:58:01 +0300

    Feedback section finished

--- js/script.js
+++ js/script.js
@@ -1,40 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const header = document.querySelector("#header");
+  const header = document.querySelector("#header");
 
-    function toggleHeaderClass() {
-        if (window.scrollY > 0) {
-            header.classList.add("active");
-        } else {
-            header.classList.remove("active");
-        }
+  function toggleHeaderClass() {
+    if (window.scrollY > 0) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
     }
+  }
 
-    toggleHeaderClass();
+  toggleHeaderClass();
 
-    window.addEventListener("scroll", toggleHeaderClass);
+  window.addEventListener("scroll", toggleHeaderClass);
 });
 
 $(document).ready(function () {
   $(".feedbacks__slider").slick({
     arrows: true,
     dots: true,
-    adaptiveHeight: false,
-    variableWidth: false,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 600,
-    easing: "linear",
-    infinite: true,
-    initialSlide: 1,
-    autoplay: false,
-    autoplaySpeed: 3000,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-    draggable: true,
-    swipe: true,
-    touchTreshold: 8,
-    touchMove: true,
-    waitForAnimate: true,
+    speed: 1000,
+    draggable: false
   });
 });
